f3341a7380b98a5242dfe8c3c85a0005a009814e
wycj2009 <wycj2009@naver.com>
2024-03-04 10:33:31 +0900

    Init Fresco

---
+++ app/src/main/java/com/example/android_fresco_example/App.kt
@@ -0,0 +1,13 @@
+package com.example.android_fresco_example
+
+import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Fresco.initialize(this)
+    }
+}
--- app/src/main/java/com/example/android_fresco_example/MainActivity.kt
+++ app/src/main/java/com/example/android_fresco_example/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android_fresco_example
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_fresco_example.databinding.ActivityMainBinding
@@ -12,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val uri: Uri = Uri.parse("https://raw.githubusercontent.com/facebook/fresco/main/docs/static/logo.png")
+            binding.simpleDraweeView.setImageURI(uri)
+        }
     }
 }
